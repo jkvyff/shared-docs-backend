1715ec11155cb9287d58295ab592b187fb2d641f
jkvyff <kristianvyff@gmail.com>
2020-03-04 16:47:24 -0800

    extra slash

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "https://www.vyff.com/"
+    origin: "https://www.vyff.com"
   })
 );
 
