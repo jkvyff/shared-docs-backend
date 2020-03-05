ebbe3127b9387d0bd9d09cd50dcb3448287b5d41
jkvyff <kristianvyff@gmail.com>
2020-03-04 16:39:28 -0800

    https

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://www.vyff.com/"
+    origin: "https://www.vyff.com/"
   })
 );
 
