6a99400e19ad1f19db0cfc21f401512ffff40345
jkvyff <kristianvyff@gmail.com>
2020-03-04 16:36:53 -0800

    update cors

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:3000"
+    origin: "http://www.vyff.com/"
   })
 );
 
