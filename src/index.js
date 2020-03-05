8b8171fce9102c9543ecbf6a2123ab7e41daefef
jkvyff <kristianvyff@gmail.com>
2020-03-05 10:23:09 -0800

    Frontend link

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "https://shared-docs-f3513.firebaseapp.com"
+    origin: `${process.env.FRONTEND}`
   })
 );
 
